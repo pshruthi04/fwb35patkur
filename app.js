b48c6247d215b0b3e864bd62dc128fdf60c42d84
pshruthi04 <89540632+pshruthi04@users.noreply.github.com>
2021-11-03 17:28:55 -0500

    small changes made

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var macbookRouter = require('./routes/macbook');
-var addmodsRouter = require('./routes/addmods')
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/macbook', macbookRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
