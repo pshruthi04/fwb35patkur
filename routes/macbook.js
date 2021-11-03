176371547ed9b237c8c41029da1d27a2491be48d
pshruthi04 <89540632+pshruthi04@users.noreply.github.com>
2021-11-03 16:53:53 -0500

    added addmods

--- routes/macbook.js
+++ routes/macbook.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results for Macbook' });
+  res.render('macbook', { title: 'Search Results for Macbook' });
 });
 
 module.exports = router;
