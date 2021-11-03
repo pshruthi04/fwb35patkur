040f02655e725026dcb4801332019af0c36143cb
pshruthi04 <89540632+pshruthi04@users.noreply.github.com>
2021-11-03 16:06:19 -0500

    added a new endpoint

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var macbookRouter = require('./routes/macbook');
+
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/macbook', macbookRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
